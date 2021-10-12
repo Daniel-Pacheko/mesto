5c3980048699eba4ec4924d782f7f5b736d68d54
Daniel Pacheco <pachekodaniel@yandex.ru>
2021-10-12 20:17:28 +0300

    Правки после первого Ревью

--- script/index.js
+++ script/index.js
@@ -2,23 +2,22 @@ const popup = document.querySelector('.popup')
 const popupCloseButton = document.querySelector('.popup__close')
 const editButton = document.querySelector('.profile__button-edit')
 
-const sendButton = document.querySelector('.popup__send')
 const form = document.querySelector('.popup__form')
 
-const nameInput = document.querySelector('.popup__name-input_fill')
-const jobInput = document.querySelector('.popup__job-input_fill')
+const nameInput = document.querySelector('.popup__input_type_name')
+const jobInput = document.querySelector('.popup__input_type_job')
 const nameProfile = document.querySelector('.profile__title')
 const jobProfile = document.querySelector('.profile__subtitle')
 
 function openPopup() {
-    popup.classList.add('popup_isOpen')
+    popup.classList.add('popup_is-open')
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
 }
 editButton.addEventListener('click', openPopup)
 
 function closePopup() {
-    popup.classList.remove('popup_isOpen')
+    popup.classList.remove('popup_is-open')
 }
 popupCloseButton.addEventListener('click', closePopup)
 
